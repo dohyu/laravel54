3e5c9ee69768daf482bce998a80b3f616717d746
FireRabbit <dohyu@naver.com>
2019-01-06 15:46:08 +0900

    exception-debugging

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -57,7 +57,9 @@ class ArticlesController extends Controller
      */
     public function show($id)
     {
-        return __METHOD__ . '은(는) 다음 기본키를 가진 Article 모델을 조회합니다.: ' . $id;
+        $article = \App\Article::findOrFail($id);
+dd($article);
+        return $article->toArray();
     }
 
     /**
