e7b937a4567f3dde03b417124f137723098d472d
FireRabbit <dohyu@naver.com>
2019-01-06 11:20:57 +0900

    event

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -38,30 +38,13 @@ class ArticlesController extends Controller
      */
     public function store(ArticlesRequest $request)
     {
-        // $rules = [
-        //     'title' => ['required'],
-        //     'content' => ['required', 'min:10'],
-        // ];
-        //
-        // $messages = [
-        //     'title.required' => '제목은 필수 입력 항목입니다.',
-        //     'content.required' => '본문은 필수 입력 항목입니다.',
-        //     'content.min' => '본문은 최소 :min 글자 이상이 필요합니다.'
-        // ];
-
-        // $validator = \Validator::make($request->all(), $rules, $messages);
-        //
-        // if ($validator->fails()) {
-        //     return back()->withErrors($validator)->withInput();
-        // }
-
-        // $this->validate($request, $rules, $messages);
-
         $article = \App\User::find(1)->articles()->create($request->all());
 
         if (! $article) {
             return back()->with('flash_message', '글이 저장되지 않았습니다.')->withInput();
         }
+
+        event(new \App\Events\ArticlesEvent($article));
 
         return redirect(route('articles.index'))->with('flash_message', '작성하신 글이 저장되었습니다.');
     }
