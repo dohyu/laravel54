9bf2f6e6ef5c3fcf912f503e1f421d34ae1d197b
FireRabbit <dohyu@naver.com>
2019-01-07 18:46:33 +0900

    documentation model

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('markdown', function () {
-    $text =<<<EOT
-# 마크다운 예제 1
+Route::get('docs/{file}', function ($file = null) {
+    $text = (new App\Documentation)->get($file);
 
-이 문서는 [마크다운][1]으로 썼습니다. 화면에는 HTML로 변환되어 출력됩니다.
-
-## 순서 없는 목록
-
-- 첫 번째 항목
-- 두 번째 항목[^1]
-
-[1]: http://daringfireball.net/projects/markdown
-
-[^1]: 두 번째 항목_ http://google.com
-EOT;
     return app(ParsedownExtra::class)->text($text);
 });
 
