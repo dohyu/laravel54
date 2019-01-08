23987e9046e1059be56abc4f4c7a062653650bd5
FireRabbit <dohyu@naver.com>
2019-01-08 12:07:09 +0900

    final-touch

--- app/Documentation.php
+++ app/Documentation.php
@@ -13,10 +13,15 @@ class Documentation
         return $this->replaceLinks($content);
     }
 
-    protected function path($file)
+    public function image($file)
     {
-        $file = ends_with($file, '.md') ? $file : $file . '.md';
-        $path = base_path('docs' . DIRECTORY_SEPARATOR . $file);
+        return \Image::make($this->path($file, 'docs/images'));
+    }
+
+    protected function path($file, $dir = 'docs')
+    {
+        $file = ends_with($file, ['.md', '.png']) ? $file : $file . '.md';
+        $path = base_path($dir . DIRECTORY_SEPARATOR . $file);
 
         if (! File::exists($path)) {
             abort(404, '요청하신 파일이 없습니다.');
@@ -28,5 +33,12 @@ class Documentation
     protected function replaceLinks($content)
     {
         return str_replace('/docs/{{version}}', '/docs', $content);
+    }
+
+    public function etag($file)
+    {
+        $lastModified = File::lastModified($this->path($file, 'docs/images'));
+
+        return md5($file . $lastModified);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('docs/{file?}', 'DocsController@show');
 
+Route::get('docs/images/{image}', 'DocsController@image')->where('image', '[\pL-\pN\._-]+-img-[0-9]{2}.png');
+
 // DB::listen(function ($query) {
 //     var_dump($query->sql);
 // });
